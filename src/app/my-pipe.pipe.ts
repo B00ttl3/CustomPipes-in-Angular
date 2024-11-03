6cb6583024b439df9b5888b85be9fc4bc3b5a2a8
B00ttl3 <mumair3825@gmail.com>
2024-11-03 23:37:07 +0500

    Learnt about how to send strong, medium and weak password messages

--- src/app/my-pipe.pipe.ts
+++ src/app/my-pipe.pipe.ts
@@ -12,9 +12,19 @@ export class MyPipePipe implements PipeTransform {
   //   return Math.pow(value , num1);
   // }
 
+  // example 2
+  // transform(value: string)
+  // {
+  //   if (value.length<11) return `Phone number is less than 11 digits. Current length is : ${value.length}`;
+  //   if(value.length>11) return `Phone number is greater than 11 digits. Current length is : ${value.length}`;
+  //   return `${value.slice(0,4)}-${value.slice(4,11)}` ;
+  // }
+
   transform(value: string)
   {
-    return `${value.slice(0,4)}-${value.slice(4,11)}` ;
+    if (value.length<7) return `Weak Password`;
+    if(value.length<10) return `Medium Password`;
+    return `Strong Password` ;
   }
 
 }
